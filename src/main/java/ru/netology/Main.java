c35b668635ec5e6c88bb0974069c3bae2b90f87d
Hopeless2 <89778966354@mail.ru>
2022-01-24 19:36:34 +0300

    Handlers added

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,8 +1,47 @@
 package ru.netology;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
         final var server = new Server();
+        server.addHandler("GET", "/classic.html", (request, responseStream) -> {
+            final var filePath = Path.of(".", "public", request.getHead());
+            final var mimeType = Files.probeContentType(filePath);
+            final var template = Files.readString(filePath);
+            final var content = template.replace(
+                    "{time}",
+                    LocalDateTime.now().toString()
+            ).getBytes();
+            responseStream.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + content.length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            responseStream.write(content);
+            responseStream.flush();
+        });
+
+        server.addHandler("GET", "standardCase", (request, responseStream) -> {
+            final var filePath = Path.of(".", "public", request.getHead());
+            final var mimeType = Files.probeContentType(filePath);
+            final var length = Files.size(filePath);
+            responseStream.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, responseStream);
+            responseStream.flush();
+        });
+
+
         server.start();
     }
 }
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,23 +1,52 @@
 package ru.netology;
 
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server extends Thread {
 
     public static final int PORT = 8080;
+    private final Map<String, Handler> handlersGet = new ConcurrentHashMap<>();//TODO выбрать map для многопотока(в качестве ключа http метод, в качестве второго ключа путь файла
+    private final Map<String, Handler> handlersPost = new ConcurrentHashMap<>();
 
     @Override
     public void run() {
         try (final var serverSocket1 = new ServerSocket(PORT)) {
             ExecutorService service = Executors.newFixedThreadPool(48);
             while (true) {
-                service.submit(new ServerThread(serverSocket1.accept()));
+                service.submit(new ServerThread(serverSocket1.accept(), this));
             }
         } catch (IOException ex) {
             ex.printStackTrace();
+        }
+    }
+
+    public void addHandler(String method, String path, Handler handler) {
+        if (method.equals("GET")) {
+            handlersGet.put(path, handler);
+        } else if (method.equals("POST")) {
+            handlersPost.put(path, handler);
+        }
+    }
+
+    public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+        if (request.getHead().equals("/classic.html")) {
+            if (request.getRequestMethod().equals("GET")) {
+                handlersGet.get(request.getHead()).handle(request, responseStream);
+            } else if (request.getRequestMethod().equals("POST")) {
+                handlersPost.get(request.getHead()).handle(request, responseStream);
+            }
+        } else {
+            if (request.getRequestMethod().equals("GET")) {
+                handlersGet.get("standardCase").handle(request, responseStream);
+            } else if (request.getRequestMethod().equals("POST")) {
+                handlersPost.get("standardCase").handle(request, responseStream);
+            }
         }
     }
 
