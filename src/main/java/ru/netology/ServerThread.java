1a2033cefdf3beeb920cff21b403f46d92ffb33c
Hopeless2 <89778966354@mail.ru>
2022-01-25 20:03:24 +0300

    Commit init

--- src/main/java/ru/netology/ServerThread.java
+++ src/main/java/ru/netology/ServerThread.java
@@ -5,20 +5,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class ServerThread implements Runnable {
     private final Socket socket;
+    private final Server server;
     private BufferedReader in = null;
     private BufferedOutputStream out = null;
     private final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
 
 
-    public ServerThread(Socket socket) {
+    public ServerThread(Socket socket, Server server) {
         this.socket = socket;
+        this.server = server;
         try {
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedOutputStream(socket.getOutputStream());
@@ -51,39 +50,8 @@ public class ServerThread implements Runnable {
                     continue;
                 }
 
-                final var filePath = Path.of(".", "public", path);
-                final var mimeType = Files.probeContentType(filePath);
-
-                // special case for classic
-                if (path.equals("/classic.html")) {
-                    final var template = Files.readString(filePath);
-                    final var content = template.replace(
-                            "{time}",
-                            LocalDateTime.now().toString()
-                    ).getBytes();
-                    out.write((
-                            "HTTP/1.1 200 OK\r\n" +
-                                    "Content-Type: " + mimeType + "\r\n" +
-                                    "Content-Length: " + content.length + "\r\n" +
-                                    "Connection: close\r\n" +
-                                    "\r\n"
-                    ).getBytes());
-                    out.write(content);
-                    out.flush();
-                    continue;
-                }
-
-                final var length = Files.size(filePath);
-                out.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + length + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                Files.copy(filePath, out);
-                out.flush();
-
+                Request request = new Request(parts[0], parts[1], parts[2]);
+                server.handle(request, out);
 
             } catch (IOException ex) {
                 ex.printStackTrace();
