df495f844a1b268edd0877e21a8f61bd8932217f
Hopeless2 <89778966354@mail.ru>
2022-01-27 12:07:35 +0300

    Commit init

--- src/main/java/ru/netology/ServerThread.java
+++ src/main/java/ru/netology/ServerThread.java
@@ -5,19 +5,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class ServerThread implements Runnable {
     private final Socket socket;
-    private final Server server;
     private BufferedReader in = null;
     private BufferedOutputStream out = null;
     private final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
 
 
-    public ServerThread(Socket socket, Server server) {
+    public ServerThread(Socket socket) {
         this.socket = socket;
-        this.server = server;
         try {
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedOutputStream(socket.getOutputStream());
@@ -50,8 +51,39 @@ public class ServerThread implements Runnable {
                     continue;
                 }
 
-                Request request = new Request(parts[0], parts[1], parts[2]);
-                server.handle(request, out);
+                final var filePath = Path.of(".", "public", path);
+                final var mimeType = Files.probeContentType(filePath);
+
+                // special case for classic
+                if (path.equals("/classic.html")) {
+                    final var template = Files.readString(filePath);
+                    final var content = template.replace(
+                            "{time}",
+                            LocalDateTime.now().toString()
+                    ).getBytes();
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + content.length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.write(content);
+                    out.flush();
+                    continue;
+                }
+
+                final var length = Files.size(filePath);
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, out);
+                out.flush();
+
 
             } catch (IOException ex) {
                 ex.printStackTrace();
